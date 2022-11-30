7f3b77bfcb599dbd8f77be3457aa7b24ced77078
RatajX <58147863+RatajXX1@users.noreply.github.com>
2022-11-30 15:43:19 +0100

    API

---
+++ server/api/config.php
@@ -0,0 +1,14 @@
+<?php
+
+$MYSQL_USER = "rootdat";
+$MYSQL_PASSWORD = "password";
+$MYSQL_DATABASE = "edu_platform";
+$MYSQL_HOST = "localhost";
+$MYSQL_PORT = 3306;
+
+
+// Email Config
+$smtp_host = "smtp.gmail.com";
+$smtp_port = 25;
+$smtp_login = "email@gmail.com";
+$smtp_password = "password";
