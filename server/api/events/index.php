82329c6862557d2858d89f72a2a1f49d79cdc7f5
RatajX <58147863+RatajXX1@users.noreply.github.com>
2022-12-03 13:50:29 +0100

    filtres on server

--- server/api/events/index.php
+++ server/api/events/index.php
@@ -9,11 +9,59 @@ require '../auth/Session.php';
 header('Content-Type: application/json');
 
 $page = $_GET['page'] ?? 0;
+// filtes
+$qury = $_GET['query'] ?? NULL;
+$client = $_GET['client'] ?? NULL;
+$state = $_GET['state'] ?? NULL;
+$from = $_GET['from'] ?? NULL;
+$to = $_GET['to'] ?? NULL;
+
+function isFilter() {
+    global $qury, $client, $state, $from, $to;
+    if (
+        $qury != NULL ||
+        $client != NULL ||
+        $state != NULL ||
+        $from != NULL ||
+        $to != NULL
+        ) return true;
+    else return false;
+}
+
+function SearchQuery() {
+    global $qury, $client, $state, $from, $to;
+    $Search = "";
+
+    if ($qury != NULL) {
+        if ($Search != "") $Search .= " AND ";
+        $Search .= " events.title like '%$qury%' ";
+    }
+
+    if ($client != NULL) {
+        if ($Search != "") $Search .= " AND ";
+        $Search .= " events.ClientID = $client ";
+    }
+
+    if ($state != NULL) {
+        if ($Search != "") $Search .= " AND ";
+        $Search .= " events.State = $state ";
+    }
+
+    if ($from != NULL && $to != NULL) {
+        if ($Search != "") $Search .= " AND ";
+        $Search .= " events.created between '$from' and '$to'";
+    }
+
+
+    return $Search;
+}
 
 $sql_cursor = sql_con();
 if (enter_to_view_by_rank($sql_cursor, 2) && is_numeric($page)) {
     $page = 25 * ($page > 0 ? $page - 1 : $page);
-    $resoult = $sql_cursor->query("SELECT events.ID,events.State,clients.DescName,events.title,events.created,events.ETA FROM events JOIN clients ON clients.ID = events.ClientID order by events.ID asc limit 25 offset $page;");
+    if (isFilter())
+        $resoult = $sql_cursor->query("SELECT events.ID,events.State,clients.DescName,events.title,events.created,events.ETA FROM events JOIN clients ON clients.ID = events.ClientID WHERE ". SearchQuery() ." order by events.ID asc limit 25 offset $page;");
+    else $resoult = $sql_cursor->query("SELECT events.ID,events.State,clients.DescName,events.title,events.created,events.ETA FROM events JOIN clients ON clients.ID = events.ClientID order by events.ID asc limit 25 offset $page;");
     if ($resoult->num_rows > 0) {
         $users = array();
         while ($row = $resoult->fetch_assoc()) {
@@ -29,7 +77,8 @@ if (enter_to_view_by_rank($sql_cursor, 2) && is_numeric($page)) {
         echo json_encode(array(
             'CODE' => 'OK',
             'Mess' => 'Pobrano dane',
-            'Users' => $users
+            'Users' => $users,
+            "Queyr" => SearchQuery(),
         ));
     } else echo json_encode(array(
         'CODE' => 'OK',
